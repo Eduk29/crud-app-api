5ce69384227631631957955c95caa31930eeca9e
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2022-07-16 22:52:57 -0300

    feat: Initialize Spring Boot Application

---
+++ src/main/java/br/com/crud/app/backend/CrudAppApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.crud.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppApiApplication.class, args);
+	}
+
+}

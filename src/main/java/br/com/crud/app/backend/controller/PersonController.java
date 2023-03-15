996f9374e4b3e6e276d0bccdf3722d9713027fd2
Eduardo Marques <edu.temarques@gmail.com>
2023-03-15 12:14:38 -0300

    refactor: fix in cors to heroku

--- src/main/java/br/com/crud/app/backend/controller/PersonController.java
+++ src/main/java/br/com/crud/app/backend/controller/PersonController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/persons")
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Api(tags = "Persons REST API")
 public class PersonController {
 
